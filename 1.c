08ce91149196a0d4db990c4e40056824a978af5c
hack-1200 <ibrox9656@gmail.com>
2016-09-27 01:50:37 +0300

    Add el

--- 1.c
+++ 1.c
@@ -1,53 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
-typedef int tInfo;
-typedef struct Node{
-	tInfo x;
-	struct Node *next;
-} node;
+#include <iostream>
+#include <fstream>
+using namespace std;
 
-void *create();
-void addToEnd(node*,tInfo);
-void delFromEnd(node*);
-void pr(node*);
+struct Node
+{
+char info;
+Node *next;
+};
 
-int main(){
-	node *root=create();
+struct LinkedList
+{
+Node *head;
+};
 
+void Push(Node *&node, char elem)
+{
+Node *p = new Node;
+p->info = elem;
+p->next = NULL;
+node->next = p;
+node = p;
+};
 
+void Dispose(Node *&node)
+{
+while(node != NULL){
+Node *p = node;
+node = node->next;
+delete(p);
 }
+};
 
-void *create(){
-	node *root;
-	root=(node*)malloc(sizeof(node));
-	root->next=NULL;
-	root->x=0;
-	return root;
+char Pop(Node *&node)
+{
+char c = node->info;
+node = node->next;
+return c;
+};
+
+int main()
+{
+fstream in("input.txt");
+ofstream out("output.txt");
+int l = 0,i = 0;
+char ch;
+Node *p_t = new Node;
+p_t->next = NULL;
+LinkedList v,t;
+t.head = p_t; v.head = NULL;
+while(!in.eof()){
+ch = in.get();
+if((ch == ' ') || (ch == '\n') || (ch == EOF)){
+if(i > l){
+l = i;
+Dispose(v.head);
+v.head = t.head;
+} else {
+Dispose(t.head);
 }
-
-void addToEnd(node *root,tInfo lx){
-	while(root->next!=NULL){
-		root=root->next;
-	}
-
-	root->next=(node*)malloc(sizeof(node));
-	root->x=lx;
-	root->next->next=NULL;
+p_t = new Node;
+p_t->next = NULL;
+t.head = p_t;
+i = -1;
+} else Push(p_t,ch);
+i++;
 }
-
-void pr(node *root){
-	while(root->next!=NULL){
-		printf("%d ",root->x);
-		root=root->next;
-	}
-	printf("\n");
+out « l « endl;
+for(i = 0; i < l; i++){
+ch = Pop(v.head->next);
+out«ch;
 }
-
-void delFromEnd(node* root){
-	while(root->next->next!=NULL){
-		root=root->next;
-	}
-	free(root->next);
-	root->next=NULL;
-	
+Dispose(t.head);
+Dispose(v.head);
+return 0;
 }
+#include "LinkedList.h"
+#include <iostream>
+
+void Push(Node *&node, char c)
+{
+Node *p = new Node;
+p->info = c;
+p->next = NULL;
+node->next = p;
+node = node->next;
+};
+
+bool Сoincide(Node *node_1, Node *node_2)
+{
+bool a = true;
+while((a) && (node_1 != NULL) && (node_2 != NULL)){
+if (node_1->info != node_2->info) a = false;
+node_1 = node_1->next; node_2 = node_2->next;
+}
+if((a) && (node_1 == NULL) && (node_2 == NULL)) a = true;
+else a = false;
+return a;
+};
+
+char Pop(Node *&node)
+{
+char c;
+c = node->info;
+node = node->next;
+return c;
+};
+
+void Dispose(Node *&node)
+{
+while(node != NULL){
+Node *p = node;
+node = node->next;
+delete(p);
+}
+};
