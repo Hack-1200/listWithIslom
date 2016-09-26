940a6f3b786c3d22654025b16e8a50b569047f17
hack-1200 <ibrox9656@gmail.com>
2016-09-27 01:18:27 +0300

    Add Islom

--- 1.c
+++ 1.c
@@ -6,8 +6,18 @@ typedef struct Node{
 	struct Node *next;
 } node;
 
+void *create(){
+	node *root;
+	root=(node*)malloc(sizeof(node));
+	root->next=NULL;
+	root->x=0;
+	return root;
+}
+
 
 int main(){
-	
+	node *root=create();
+
 
 }
+
